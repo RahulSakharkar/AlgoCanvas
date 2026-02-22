86671c05d729ff7d9f9c05e8fbd5c0b6e088488c
Rahul Sakharkar <rahul.sakharkar@e2open.com>
2026-02-22 23:44:06 +0530

    deployment fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 //  @type {import('next').NextConfig}
 const nextConfig = {
-    images: {
-        unoptimized: true
-    },
-    output: 'export', // Outputs a Single-Page Application (SPA).
-    distDir: './build', // Changes the build output directory to `./dist`.
-    assetPrefix: '/AlgorithmVisualizer',
-    basePath: '/AlgorithmVisualizer',
-}
-   
+  images: {
+    unoptimized: true,
+  },
+  output: 'export', // Outputs a Single-Page Application (SPA).
+  distDir: './build', // Changes the build output directory to `./dist`.
+  assetPrefix: '/AlgoCanvas',
+  basePath: '/AlgoCanvas',
+};
+
 export default nextConfig;
